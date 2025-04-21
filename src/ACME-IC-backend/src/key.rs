d76c03bba4cf798a9b6df940d0a64309eed1dd43
zianksm <mynameiszian@gmail.com>
2025-04-21 16:22:25 +0800

    chore: placeholder

--- src/ACME-IC-backend/src/key.rs
+++ src/ACME-IC-backend/src/key.rs
@@ -1,1 +1,20 @@
-pub struct KeyPair;
+use x509_cert::spki;
+
+pub struct AcmeSigningKey;
+
+#[derive(Clone, Debug)]
+pub struct AcmeVerifyingKey;
+
+impl spki::EncodePublicKey for AcmeVerifyingKey {
+    fn to_public_key_der(&self) -> spki::Result<spki::Document> {
+        todo!()
+    }
+}
+
+impl signature::Keypair for AcmeSigningKey {
+    type VerifyingKey = AcmeVerifyingKey;
+
+    fn verifying_key(&self) -> Self::VerifyingKey {
+        todo!()
+    }
+}

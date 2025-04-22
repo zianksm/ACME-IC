bd3e150e14b3e79af73238014b4bcffdd18b7095
zianksm <mynameiszian@gmail.com>
2025-04-22 16:36:31 +0800

    feat: added more placeholder

--- src/ACME-IC-backend/src/key.rs
+++ src/ACME-IC-backend/src/key.rs
@@ -1,11 +1,15 @@
-use std::{cell::RefCell, rc::Rc, sync::Arc};
+use std::{cell::RefCell, rc::Rc, str::FromStr, sync::Arc};
 
 use ic_cdk::api::management_canister::ecdsa::{
     self, ecdsa_public_key, sign_with_ecdsa, EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyResponse,
 };
+use ic_stable_structures::Storable;
 use k256::{elliptic_curve::PublicKey, Secp256k1};
 use tiny_keccak::{Hasher, Keccak};
-use x509_cert::{builder::Profile, spki};
+use x509_cert::{builder::Profile, certificate::CertificateInner, der::Encode, name::Name, spki};
+
+const ROOT_NAME: &'static str = "CN=IC ENCRYPT";
+const ROOT_SERIAL_NUMBER: u128 = 0;
 
 enum EcdsaKeyIds {
     #[allow(unused)]
@@ -32,12 +36,16 @@ impl EcdsaKeyIds {
 
 #[derive(Clone, Debug)]
 pub struct AcmeKey {
-    domains: Vec<String>,
+    domain: Name,
+    serial_number: u128,
 }
 
 impl AcmeKey {
-    pub fn new(domains: Vec<String>) -> Self {
-        Self { domains }
+    pub fn new(domain: Name, serial_number: u128) -> Self {
+        Self {
+            domain,
+            serial_number,
+        }
     }
 
     pub fn id(&self) -> Vec<u8> {
@@ -45,18 +53,20 @@ impl AcmeKey {
 
         let mut hasher = Keccak::v512();
 
-        let concat = self
-            .domains
-            .clone()
-            .into_iter()
-            .reduce(|p, s| p + &s)
-            .unwrap();
+        self.domain.encode_to_slice(&mut buff).unwrap();
 
-        hasher.update(concat.as_bytes());
+        hasher.update(&buff);
+        hasher.update(&self.serial_number.to_bytes_checked());
+
+        buff.clear();
 
         hasher.finalize(&mut buff);
 
         buff
+    }
+
+    pub fn is_root(&self) -> bool {
+        self.domain.is_empty()
     }
 }
 
@@ -98,22 +108,39 @@ impl signature::Keypair for AcmeKey {
     }
 }
 
-pub struct TlsCertificate {
+pub struct Certificate {
     key: AcmeKey,
 }
 
-impl TlsCertificate {
+impl Certificate {
     pub fn root() -> Self {
         Self {
-            key: AcmeKey::new(vec![]),
+            key: AcmeKey::new(vec![], ROOT_SERIAL_NUMBER),
         }
     }
 
+    pub fn root_name() -> Name {
+        Name::from_str(ROOT_NAME)
+    }
+
     pub fn profile(&self) -> Profile {
-        todo!()
+        if self.key.is_root() {
+            return Profile::Root;
+        }
+
+        // we dont support subCA certificate for now
+        Profile::Leaf {
+            issuer: Self::root_name(),
+            enable_key_agreement: true,
+            enable_key_encipherment: true,
+        }
     }
 
     pub fn build_leaf() -> Self {
+        // get serial number from somewhere
+        // determine vailidity -> findout how, hardcode to 1 year for now
+        // subject is the acme key domain
+
         todo!()
     }
 

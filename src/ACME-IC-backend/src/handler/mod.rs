7a5bc703f8ee38d2f333cb87bea3083518864b08
zianksm <mynameiszian@gmail.com>
2025-04-28 13:56:13 +0800

    feat: add basic boilerplate code

--- src/ACME-IC-backend/src/handler/mod.rs
+++ src/ACME-IC-backend/src/handler/mod.rs
@@ -1,1 +1,33 @@
+use ic_http_certification::{HttpRequest, HttpResponse, HttpUpdateRequest, HttpUpdateResponse};
+
+pub type UpdateResponse<'a> = HttpUpdateResponse<'a>;
+pub type RegularResponse<'a> = HttpResponse<'a>;
+pub type UpdateRequest<'a> = HttpUpdateRequest<'a>;
+pub type RegularRequest<'a> = HttpRequest<'a>;
+
 mod types;
+
+pub trait RequestMarker {
+    type Response: ResponseMarker;
+}
+
+pub trait ResponseMarker {}
+
+impl<'a> RequestMarker for UpdateRequest<'a> {
+    type Response = UpdateResponse<'a>;
+}
+
+impl<'a> ResponseMarker for UpdateResponse<'a> {}
+
+impl<'a> RequestMarker for RegularRequest<'a> {
+    type Response = RegularResponse<'a>;
+}
+impl<'a> ResponseMarker for RegularResponse<'a> {}
+
+pub trait Handler {
+    const PATH: &'static str;
+
+    type Request: RequestMarker;
+
+    fn handle(req: Self::Request) -> <Self::Request as RequestMarker>::Response;
+}
